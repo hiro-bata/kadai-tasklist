635b75a745aaadd405a60066746c018b79c15a3d
hiro.bata <kawa.060313@gmail.com>
2020-08-16 22:56:22 +0000

    add column/top page

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,19 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
-  get 'users/new'
-  get 'users/create'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'toppages/index'
+    root to: 'toppages#index'
+    
+    get 'users/index'
+    get 'users/show'
+    get 'users/new'
+    get 'users/create'
     root to: "tasks#index"
+    
+    get 'login', to: 'sessions#new'
+    post 'login', to: 'sessions#create'
+    delete 'logout', to: 'sessions#destroy'
     
     get "signup", to: "users#new"
     resources :users, only: [:index, :show, :new, :create]
     resources :tasks
+    
 end
